cc2b6d5d8fb4206554beebf9aee490f450e961b0
Husnain <m.husnainmehmood@gmail.com>
2020-10-10 17:38:18 +0500

    Full Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Header from "./components/header/header";
+import Generator from "./components/meme-generator/meme-generator";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Generator />
+    </div>
+  );
+}
+
+export default App;
